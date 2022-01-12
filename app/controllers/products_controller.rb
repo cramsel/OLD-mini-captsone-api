80522eed5c72ad9b9caa1bce25b917ffa2422b78
cramsel <Luxicorde@gmail.com>
2022-01-11 19:10:57 -0600

    Add Individual Product Routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,16 @@ class ProductsController < ApplicationController
   def display_all
     render json: Product.all
   end
+
+  def display_water_bottle
+    render json: Product.find_by(name: "Water Bottle")
+  end
+
+  def display_bicycle
+    render json: Product.find_by(name: "Bicycle")
+  end
+
+  def display_toolbox
+    render json: Product.find_by(name: "Toolbox")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-get "/display_all", controller: "products", action: "display_all"
+  get "/display_all", controller: "products", action: "display_all"
+  get "/display_water_bottle", controller: "products", action: "display_water_bottle"
+  get "/display_bicycle", controller: "products", action: "display_bicycle"
+  get "/display_toolbox", controller: "products", action: "display_toolbox"
 end
